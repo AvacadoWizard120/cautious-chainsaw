2b00cf9ed5a2abad6540e9d7dc38f959bb2ae44b
Uriasej <uriasej@owensborocatholic.us>
2024-05-06 17:07:16 -0500

    Level framework

--- main.py
+++ main.py
@@ -24,8 +24,49 @@ print("Remaining Cards: " + str(deck.get_remaining_cards()))
 print("Troop cards in deck: " + str(deck.get_card_count("Troops")))
 print("Restock cards in deck: " + str(deck.get_card_count("Restock")))
 
-# I'm thinking maybe we have a dictionary system that allows players to create 'Units' or 'Squads'
-# I don't know what to call them, but essentially we have cards with different types of Units and you can use
-# those cards to create a Squad or wtv we end up calling it, and then the player can use the squad to try and
-# take a trench or make them hold a trench segment and depending on the types of units in the squad, the chances
-# of victory in battle will increase/decrease.
+# Scratch everything I said in this spot, let's keep things simple for the sake
+# of fun.
+
+
+# Set the initial level at turn 0
+game_level = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+unit_data = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0 ,0]
+]
+
+# define some symbols so we can display graphics
+# the size will be how many units will fit in the trench.
+trench_data = {
+    1: {'symbol': '|', 'size': 1}, # 1 is single digit number of units [0,9]
+    2: {'symbol': '|', 'size': 2}, # 2 is double digit number of units [0,99]
+    3: {'symbol': '|', 'size': 3} # [0,999]
+}
+
+
+
+# You can then iterate over the game_level array and print the appropriate symbol based on the trench_data and unit_data
+for row in range(len(game_level)):
+    for col in range(len(game_level[row])):
+        if game_level[row][col] in trench_data:
+            symbol = trench_data[game_level[row][col]]['symbol']
+            size = trench_data[game_level[row][col]]['size']
+            units = unit_data[row][col]
+            print(f'{symbol}{(size - 1) * " "}{units}{symbol}', end='')
+        else:
+            print(' ' * 5, end='')  # Print 5 spaces for non-trench tiles
+    print()  # Print a new line after each row
+
+
+user_input = input("Press Enter to continue...")
+print("You pressed Enter!")
---
+++ run-windows.bat
@@ -0,0 +1,1 @@
+start powershell -Command "python main.py"
